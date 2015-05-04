365c59e5416469548c9a8f33bcc8e763d75b914f
MenzorovaKsenia <menzorova@phystech.edu>
2015-05-04 13:40:09 +0300

    Mesh с VBO

--- sources/Mesh.cs
+++ sources/Mesh.cs
@@ -18,30 +18,30 @@ using OpenTK.Platform;
 namespace DrawHeightmapGL
 {
 	
-//=============================================================
-//==================Class_Mesh=================================
-//=============================================================
-//	Library Open.TK.Math helps us to use arrays easily. We can use type vector, which is needed for loading data. 
+	//=============================================================
+	//==================Class_Mesh=================================
+	//=============================================================
+	//	Library Open.TK.Math helps us to use arrays easily. We can use type vector, which is needed for loading data. 
     public class Mesh
     {
         int MAP_SIZE_X = 3840;     // Size of the vertex map
         int MAP_SIZE_Z = 3840;     // Size of the vertex map
         short[] g_HeightMap;    // Array which contains coordinates of all vertexes
 
-        public int m_nVertexCount = 0;	 //number of vertexes (Vertex count)						
-        public OpenTK.Math.Vector3[] m_pVertices; //Vertex Data								
+        public int m_nVertexCount = 0;	//number of vertexes (Vertex count)							
+        public OpenTK.Math.Vector3[] m_pVertices; // Vertex Data								
         public OpenTK.Math.Vector3[] m_pRGB; // Texture Coordinates
-        public OpenTK.Math.Vector2[] m_pTexCoords; // Texture Coordinates							
+        public OpenTK.Math.Vector2[] m_pTexCoords; // Texture Coordinates								
 
-    	public uint	m_nTextureId;				// Texture ID
-        //особенность openGL. VBO дали существенный прирост производительност над непосредственном режимом визуализации 
+    	public uint	m_nTextureId; // Texture ID								
+ //особенность openGL. VBO дали существенный прирост производительност над непосредственном режимом визуализации 
 	    // Vertex Buffer Object Names
 	    public uint	m_nVBOVertices;								// Vertex VBO Name
 	    public uint	m_nVBOTexCoords;							// Texture Coordinate VBO Name
 	    public uint	m_nVBORGB;							// Texture Coordinate VBO Name
 
-	    // Temporary Data
-        //Bind a named texture to a texturing target
+		    // Temporary Data
+	        //Bind a named texture to a texturing target
         public uint TMU0_Handle;
         public OpenTK.Graphics.OpenGL.TextureTarget TMU0_Target;
 
@@ -49,8 +49,7 @@ namespace DrawHeightmapGL
         public uint TMU1_Handle;
         public OpenTK.Graphics.OpenGL.TextureTarget TMU1_Target;
 
-		
-		//Width, Heigth of our MAP
+//Width, Heigth of our MAP
         int imageWidth, imageHeight;
 
         public float maxX = 0;
@@ -61,12 +60,13 @@ namespace DrawHeightmapGL
         public float minZ = 0;
 
         public Mesh()
-        {	//memory
+        {
+	//memory
             g_HeightMap = new short[MAP_SIZE_X*MAP_SIZE_Z];
         }
 
         private void LoadHeightmap(string filename)
-        {	//Класс FileStream предоставляет реализацию абстрактного члена Stream в манере, подходящей для потоковой работы с файлами. Это элементарный поток, и он может записывать или читать только один байт или массив байтов.
+        {//Класс FileStream предоставляет реализацию абстрактного члена Stream в манере, подходящей для потоковой работы с файлами. Это элементарный поток, и он может записывать или читать только один байт или массив байтов.
             using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
 //Класс BinaryReader служит оболочкой, в которую заключается байтовый поток, управляющий вводом двоичных данных. Ниже приведен наиболее часто употребляемый конструктор этого класса:BinaryReader(Stream input)
 //где input обозначает поток, из которого вводятся считываемые данные. Для чтения из входного файла в качестве параметра input может быть указан объект, создаваемый средствами класса FileStream.
@@ -77,26 +77,26 @@ namespace DrawHeightmapGL
                 {
                     for (int i = 0; i < MAP_SIZE_X * MAP_SIZE_Z; i++)
                     {
-						//ReadInt - считываем из текущего потока целое число со знаком длиной два байта и перемещаем текущую позицию в потоке на два байта вперед. try-catch если можем, то делаем то, что под траем, если нет- кэтчим ошибку.
+	//ReadInt - считываем из текущего потока целое число со знаком длиной два байта и перемещаем текущую позицию в потоке на два байта вперед. try-catch если можем, то делаем то, что под траем, если нет- кэтчим ошибку.
                         g_HeightMap[i] = br.ReadInt16();
                     }
                 }
                 catch (Exception ex)
                 {
-                   	//возвращает название ошибки.
-					 MessageBox.Show(ex.ToString());
+	//возвращает название ошибки.
+                    MessageBox.Show(ex.ToString());
                 }
             }
         }
-
-		//загрузка текстур
+//загрузка текстур
         private void LoadTexture(string filename)
         {
-		//здесь будет функция загрузки текстур.
+            
+            ImageGDI.LoadFromDisk("4.bmp", out TMU0_Handle, out TMU0_Target, out imageWidth, out imageHeight);
         }
-		//ффункция пересчёта данных -масштабирование
+//ффункция пересчёта данных -масштабирование
 		//устанавливаем разрешение
-         public void Calculate(float flHeightScale, float flResolution)
+        public void Calculate(float flHeightScale, float flResolution)
         {
             // Generate Vertex Field
             m_nVertexCount = (int)(MAP_SIZE_X * MAP_SIZE_Z * 6 / (flResolution * flResolution));
@@ -105,18 +105,39 @@ namespace DrawHeightmapGL
             m_pRGB = new OpenTK.Math.Vector3[m_nVertexCount];
             m_pTexCoords = new OpenTK.Math.Vector2[m_nVertexCount];				// Allocate Tex Coord Data
 
-            int nX, nZ = 0;									// Create Variables
+            int nX, nZ, nTri, nIndex = 0;									// Create Variables
             float flX, flZ;
             for (nZ = 0; nZ < MAP_SIZE_Z - 1; nZ += (int)flResolution)
             {
                 for (nX = 0; nX < MAP_SIZE_X - 1; nX += (int)flResolution)
                 {
-          //перерасчёт масштаба
-                    
+                    for (nTri = 0; nTri < 6; nTri++)
+                    {
+                        if (nIndex < m_nVertexCount)
+                        {
+                            // Using This Quick Hack, Figure The X,Z Position Of The Point
+                            flX = (float)nX + ((nTri == 1 || nTri == 2 || nTri == 5) ? flResolution : 0.0f);
+                            flZ = (float)nZ + ((nTri == 2 || nTri == 4 || nTri == 5) ? flResolution : 0.0f);
+
+                            m_pVertices[nIndex] = new OpenTK.Math.Vector3();
+                            // Set The Data, Using PtHeight To Obtain The Y Value
+                            m_pVertices[nIndex].Z = (flX - (MAP_SIZE_X / 2)) * 10;
+                            m_pVertices[nIndex].Y = ((GetHeight(g_HeightMap, (int)flX, (int)flZ)) * flHeightScale) * 10;
+                            m_pVertices[nIndex].X = (flZ - (MAP_SIZE_Z / 2)) * 10;
+
+                            m_pTexCoords[nIndex] = new OpenTK.Math.Vector2();
+                            // Stretch The Texture Across The Entire Mesh
+                            m_pTexCoords[nIndex].X = flX / imageWidth;
+                            m_pTexCoords[nIndex].Y = flZ / imageHeight;
+
+                        }
+                        nIndex++;
+                    }
                 }
             }
         }
-		//загрузка и обработка всего и вся.
+//загрузка и обработка всего и вся.
+
         public void Load(string binariFileName, string texFileName, float flHeightScale, float flResolution)
         {
             LoadHeightmap(binariFileName);
@@ -125,16 +146,39 @@ namespace DrawHeightmapGL
 
             Calculate(flHeightScale, flResolution);
         }
-		//ищем высоту из карты вершин
+
         short GetHeight(short[] pHeightMap, int X, int Y)      // Возвращает высоту из карты вершин
         {
-            int x = X % MAP_SIZE_X;          
-            int y = Y % MAP_SIZE_Z;          
+            int x = X % MAP_SIZE_X;          // Проверка переменной х
+            int y = Y % MAP_SIZE_Z;          // Проверка переменной y
 
             return pHeightMap[x + (y * MAP_SIZE_X)];      // Возвращаем значение высоты
         }
-			
-		//Подставка для карты
+
+        public void BuildVBOs()
+        {
+            // Generate And Bind The Vertex Buffer
+            GL.DeleteBuffers(1, ref m_nVBOVertices);							// Get A Valid Name
+            GL.GenBuffers(1, out m_nVBOVertices);							// Get A Valid Name
+            GL.BindBuffer(BufferTarget.ArrayBuffer, m_nVBOVertices);			// Bind The Buffer
+            // Load The Data
+            GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(m_nVertexCount * 3 * sizeof(float)), m_pVertices, BufferUsageHint.StaticDraw);
+            
+            // Generate And Bind The Color Buffer
+            GL.DeleteBuffers(1, ref m_nVBORGB);							// Get A Valid Name
+            GL.GenBuffers(1, out m_nVBORGB);							// Get A Valid Name
+            GL.BindBuffer(BufferTarget.ArrayBuffer, m_nVBORGB);			// Bind The Buffer
+            // Load The Data
+            GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(m_nVertexCount * 3 * sizeof(float)), m_pRGB, BufferUsageHint.StaticDraw);
+
+            // Generate And Bind The Texture Coordinate Buffer
+            GL.DeleteBuffers(1, ref m_nVBOTexCoords);							// Get A Valid Name
+            GL.GenBuffers(1, out m_nVBOTexCoords);							// Get A Valid Name
+            GL.BindBuffer(BufferTarget.ArrayBuffer, m_nVBOTexCoords);		// Bind The Buffer
+            // Load The Data
+            GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(m_nVertexCount * 2 * sizeof(float)), m_pTexCoords, BufferUsageHint.StaticDraw);
+        }
+
         public void DrawRectengle()
         {
             #region  rectangle
@@ -179,8 +223,7 @@ namespace DrawHeightmapGL
 
             GL.Vertex3(minX - 1000, minY - 1000, maxZ + 1000);
             GL.Vertex3(minX - 1000, maxY + 1000, maxZ + 1000);
-           
-
+          
             //  Нижняя грань
             GL.Vertex3(minX - 1000, minY - 1000, minZ - 1000);
             GL.Vertex3(maxX + 1000, minY - 1000, minZ - 1000);
@@ -224,7 +267,7 @@ namespace DrawHeightmapGL
         {
             #region  rectangle
             GL.Begin(BeginMode.Quads);
-			//числа приблизительные по высмотренным значениям из файла
+
             //  задняя грань
             GL.Color3(Color.FromArgb(23, 32, 89));
             GL.Vertex3(minX-6000, maxY+6000, minZ-6000);
@@ -234,6 +277,8 @@ namespace DrawHeightmapGL
             GL.Vertex3(maxX+6000, minY-6000, minZ-6000);
             GL.Color3(Color.FromArgb(23, 32, 89));
             GL.Vertex3(minX-6000, minY-6000, minZ-6000);
+
+           
 
             //  Нижняя грань
             GL.Color3(Color.FromArgb(99, 109, 206));
